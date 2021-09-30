aab2c10c35c77216d6946a9f83c6536ab6e2cdc7
Stephen Koch <stephen.koch96@gmail.com>
2021-09-30 18:30:29 -0400

    Initial commit.

---
+++ src/banking/Banking.java
@@ -0,0 +1,18 @@
+package banking;
+
+/*
+ * @author sak5680
+ */
+public class Banking {
+
+    public static void main(String[] args) {
+        BankAccount ba1 = new BankAccount();
+        BankAccount ba2 = new BankAccount();
+        
+        ba1.showStrings();
+        ba2.showStrings();
+//        System.out.println(ba1.accountNumber);
+
+    }
+    
+}
